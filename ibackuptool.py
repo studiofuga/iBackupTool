8f8d2a16755a14aa113f379d9d7cb202e58acfc0
Federico Fuga <fuga@studiofuga.com>
2020-11-21 12:35:56 +0100

    Extracts all directories

--- ibackuptool.py
+++ ibackuptool.py
@@ -1,6 +1,28 @@
 #! /usr/bin/env python
 from ibackuptool import ibackup
 from argparse import ArgumentParser
+import os
+from pathlib import Path
+
+
+class IBackupToolApp:
+    def __init__(self):
+        pass
+
+    def reconstruct(self, bck :ibackup, outdir:str):
+        try:
+            Path(outdir).mkdir(parents=True)
+        except FileExistsError:
+            # If already exists, skip it
+            pass
+        bck.open()
+        dirs = bck.get_all_directories()
+        for dir in dirs:
+            print("Extracting Dir: {}".format(dir))
+            try:
+                Path(os.path.join(outdir, dir)).mkdir(parents=True)
+            except FileExistsError:
+                pass
 
 
 if __name__ == "__main__":
@@ -8,9 +30,14 @@ if __name__ == "__main__":
         description=''
     )
     parser.add_argument('backup', help='the path of the backup to work on')
+    parser.add_argument('--unpack', help='Unpack the backup into a File System directory using symlinks')
     args = parser.parse_args()
 
     bkp = ibackup.IBackup(args.backup)
     if not bkp.check_lock():
         raise Exception('Database is writable')
 
+    app = IBackupToolApp()
+
+    if args.unpack is not None:
+        app.reconstruct(bkp, args.unpack)
--- ibackuptool/ibackup.py
+++ ibackuptool/ibackup.py
@@ -1,5 +1,5 @@
 import os
-import stat
+import sqlite3
 
 
 class IBackup:
@@ -9,10 +9,25 @@ class IBackup:
         self.path = path
 
     def check_lock(self):
-        '''
+        """
         Check if the backup is "locked" / Read Only by checking the permissions on dhe Manifest.db file
         :return: true if the backup is read only, false if it is writable
-        '''
-        if os.access(os.path.join(self.path, self._DBFILE), os.W_OK):
+        """
+        if os.access(self._manifest_file(), os.W_OK):
             return False
         return True
+
+    def open(self):
+        url = "file:{}?mode=ro&immutable=1".format(self._manifest_file())
+        print("Opening: {}".format(url))
+        self.db = sqlite3.connect(url, uri=True)
+
+    def get_all_directories(self):
+        sql = "SELECT relativePath FROM Files WHERE flags=2 AND relativePath != ''"
+        cursor = self.db.execute(sql)
+        all = cursor.fetchall()
+        return (a[0] for a in all)
+
+
+    def _manifest_file(self):
+        return os.path.join(self.path, self._DBFILE)
